b4ca3a2a7b22f8507ed0195b95f51222ab442c03
Crest <crest@cyb0rg.org>
2009-08-26 18:01:31 +0200

    finally a readme

--- main.c
+++ main.c
@@ -2,9 +2,26 @@
 #include "sensor.h"
 #include "tick.h"
 
+#define ever (;;)
+
+// WARNING: doesn't work for pointers!
+#define bzero(p,n) memset( (p), 0, (n) )
+
 int main() {
 	init_pwm();
 	init_sensor();
 	init_tick();
 	
+	init_sensor_buffer();	
+	
+	while ( !booted ) {
+		booted = spin_up_rotor();
+	}
+	
+	for ever {
+		read_sensors();
+		calculate_average();
+		choose_path();
+		follow_path();
+	}
 }
